3b53448573032ebac09b97e3845067595620bab6
Molly <donoh95@uw.edu>
2016-12-13 23:52:00 -0800

    category api dynamically fetches cats

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import sys
 sys.path.insert(0, 'lib')
 
 from bs4 import BeautifulSoup
-
+from google.appengine.api import memcache
 
 ### Utility functions you may want to use
 def pretty(obj):
@@ -32,11 +32,13 @@ def safeGet(url):
 # use handler to look at paramter that comes from url, show what products
 # comes from hardcoded url hyperlinks
 # pass in type of clothing you want results for, returns 10 items based on popularity on the site
-def getClothes(type):
+#from Products API
+def getClothes(type, numLim):
     base_url = "http://api.shopstyle.com/api/v2/products"
     apiKey = "pid=uid7936-31095456-22"
     filters = "&cat=%s&sort=Popular" % (type)
-    clothes_request = "%s?%s%s" % (base_url, apiKey, filters)
+    limit = "&limit=%s" % (numLim)
+    clothes_request = "%s?%s%s%s" % (base_url, apiKey, filters, limit)
     print (clothes_request)
     safe_request = safeGet(clothes_request)
     if not safe_request == None:
@@ -44,42 +46,80 @@ def getClothes(type):
         # print pretty(json.loads(clothes_request_json))
         return json.loads(clothes_request_json)
 
+# since the product id is not a parameter, i use a different REST call
+
+# returns a single product object if given Product id
+# from Products API
+def getSingle(id):
+    base_url = "http://api.shopstyle.com/api/v2/products"
+    apiKey = "pid=uid7936-31095456-22"
+    clothes_request = "%s/%s?%s" % (base_url, id, apiKey)
+    print (clothes_request)
+    safe_request = safeGet(clothes_request)
+    if not safe_request == None:
+        clothes_request_json = safe_request.read()
+        # print pretty(json.loads(clothes_request_json))
+        return json.loads(clothes_request_json)
+# http://api.shopstyle.com/api/v2/products/359131344?pid=YOUR_API_KEY
+
+
+# from Categories API
+def getCategories():
+    #http: // api.shopstyle.com / api / v2 / categories?pid = YOUR_API_KEY
+    url = "http://api.shopstyle.com/api/v2/categories?pid=uid7936-31095456-22&cat=women&depth=2"
+    logging.info(url)
+    cats = safeGet(url)
+    if not cats == None:
+        cats_request_json = cats.read()
+        return json.loads(cats_request_json)
 
 def getRating(brand):
-    sbrand = brand.split()
-    addBrand = ""
-    # separate multiword brands with + signs for url
-    if len(sbrand) > 1:
-        for i in range(0, len(sbrand) - 1):
-            addBrand = sbrand[i] + "+"
-        addBrand = addBrand + sbrand[len(sbrand) - 1]
-    else:  # single word brand
-        addBrand = brand
-    url = "https://rankabrand.org/sustainable-luxury-brands/" + addBrand
-    #print url
-    htmlstring = safeGet(url)
-    if not htmlstring == None:  # there is an entry for x brand
-        soup = BeautifulSoup(htmlstring, 'html.parser')
-        scoreSpan = soup.find("span", {"class": "score"})
-        if not scoreSpan == None:
-            temp = unicode((scoreSpan.a).contents[0])
-            temp = temp.encode('ascii', 'replace')
-            return temp.split("?")[0]
-        else:  # brand has an entry but no score
-            return 1
-    else:  # brand not entered on the sight
-        return 0
+
+    data = memcache.get(key=brand)
+    if data is not None:
+        logging.info("from memcash: " + str(data))
+        return data
+    else:
+        sbrand = brand.split()
+        addBrand = ""
+        val = ""
+        # separate multiword brands with + signs for url
+        if len(sbrand) > 1:
+            for i in range(0, len(sbrand) - 1):
+                addBrand = sbrand[i] + "+"
+            addBrand = addBrand + sbrand[len(sbrand) - 1]
+        else:  # single word brand
+            addBrand = brand
+
+        url = "https://rankabrand.org/sustainable-luxury-brands/" + addBrand
+        htmlstring = safeGet(url)
+        if not htmlstring == None:  # there is an entry for x brand
+            soup = BeautifulSoup(htmlstring, 'html.parser')
+            scoreSpan = soup.find("span", {"class": "score"})
+            if not scoreSpan == None:
+                temp = unicode((scoreSpan.a).contents[0])
+                temp = temp.encode('ascii', 'replace')
+                val = temp.split("?")[0]
+            else:  # brand has an entry but no score
+                val =  1
+        else:  # brand not entered on the sight
+            val =  0
+        memcache.add(key=brand, value=val, time=3600)
+        return val
+
+    #ANN TAYLOR and OLD NAVY brand names are not showin up
 
 
 # pass a single dictionary to this and make a product object
 class Product():
     """ A singular item of clothing """
-
     def __init__(self, product):
         self.imgSrc = product["image"]["sizes"]["Large"]["url"]
         self.name = product["name"].encode('ascii', 'replace')
+        self.brandedName = product["brandedName"].encode('ascii', 'replace')
         self.price = product["price"]
         self.retailer = product["retailer"]["name"].encode('ascii', 'replace')
+        self.description = product["description"]
         # if a brand has a special character, it may not be included in json
         if "brand" in product:
             self.brand = product["brand"]["name"].encode('ascii', 'replace')
@@ -87,6 +127,9 @@ class Product():
         else:
             self.brand = ""
             self.brandRating = 0
+        numfilled = round(int(self.brandRating) / 6.0)
+        self.filled = int(numfilled)
+        self.unfilled = 6 - int(numfilled)
         self.locale = product["locale"]
         self.url = product["clickUrl"]
         self.id = product["id"]
@@ -95,11 +138,21 @@ class Product():
     def __str__(self):
         return ("%s, $%s, Retailer: %s" % (self.name, str(self.price), self.retailer))
 
+class Category():
+    """"A category that exists on ShopStyle"""
+    def __init__(self, category):
+        self.name = category["name"]
+        self.id = category["id"]
+        firstitem_data = getClothes(self.id, 1)
+        #currently displays the most popular item, not the most popular and "greenest"
+        firstitem = firstitem_data["products"][0]
+        #firstitem = Product(firstitem)
+        self.imgSrc = firstitem["image"]["sizes"]["Large"]["url"]
 
-
+# sort a list of Product objects based on their brandRating and return the sorted list
 def sortClothes(type):
     masterd = {}
-    clothes_data = getClothes(type)
+    clothes_data = getClothes(type, 20)
     # the json response data containing ten clothes items of a specified type
     allClothes = clothes_data["products"]
     objectList = []
@@ -111,17 +164,9 @@ def sortClothes(type):
     masterd[type] = sortClothes
     return masterd
 
-# for cat in masterd:
-# for item in masterd[cat]:
-# print item.name + ": " + str(item.brandRating)
-
 # masterd =  {'sweaters': ['Product1', 'Proudct2', 'Product3 '],
 # 'jeans' : ['Product1', 'Proudct2', 'Product3 '],
 #  'tops' : ['Product1', 'Proudct2', 'Product3 '],}
-
-# cannot get the individual product items to print as strings
-
-
 JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
                                        extensions=['jinja2.ext.autoescape'],
                                        autoescape=True)
@@ -129,39 +174,63 @@ JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.di
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        logging.info("In MainHandler")
         tvals = {}
-        masterd = sortClothes("shoes")
+        masterd = {}
+        # get all the general categories of clothes in women's fashion
+        response = getCategories()
+        allCats = response["categories"]
+        objectList = []
+        for single in allCats:
+            #make Category instances with category name and image url
+            newitem =  Category(single)
+            objectList.append(newitem)
+        masterd["cats"] = objectList # masterd["cats"] is a list of category objects
         tvals['md'] = masterd
         template = JINJA_ENVIRONMENT.get_template('homepagetemplate.html')
         self.response.write(template.render(tvals))
 
 class ProductsHandler(webapp2.RequestHandler):
     def get(self):
-        # call getCloth
         cat = self.request.get("cat")
-        logging.info(cat)
-        # getClothes(cat)
-        logging.info("In ProductsHandler")
         tvals = {}
-
         masterd = sortClothes(cat)
-
         tvals['md'] = masterd
         template = JINJA_ENVIRONMENT.get_template('productpagetemplate.html')
         self.response.write(template.render(tvals))
 
 class SingleHandler(webapp2.RequestHandler):
     def get(self):
-        pid = self.request.get("pid")
-        logging.info(cat)
-        logging.info("in Single Handler")
-        # do i want to make another request or just use the information from before?
+        pid = self.request.get("id")
+        tvals = {}
+        data = getSingle(pid)
+        data = Product(data)
+        logging.info(data)
+        masterd = {}
+        masterd[id] = data
+        tvals['md'] = masterd
+        template = JINJA_ENVIRONMENT.get_template('singleproducttemplate.html')
+        self.response.write(template.render(tvals))
 
-# for all URLs except alt.html, use MainHandler
+class WhyHandler(webapp2.RequestHandler):
+    def get(self):
+        logging.info("in Why Handler")
+        template = JINJA_ENVIRONMENT.get_template('whypage.html')
+        self.response.write(template.render())
+
+class WhatHandler(webapp2.RequestHandler):
+    def get(self):
+        logging.info("in What Handler")
+        template = JINJA_ENVIRONMENT.get_template('whatpage.html')
+        self.response.write(template.render())
+
+
+
 application = webapp2.WSGIApplication([ \
                                         ('/' , MainHandler),
                                         ('/products', ProductsHandler),
+                                        ('/id', SingleHandler),
+                                        ('/why', WhyHandler),
+                                        ('/what', WhatHandler),
                                         ('/.*', MainHandler)
                                      #'/', #generates homepage # this should be new code to use the cat api
                                      #'/products'   , ProductsHandler # this should be what is currently in te main handler
